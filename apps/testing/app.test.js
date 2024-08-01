1e168a663840592ad65e90a7daded1741987116c
lgrs13 <lathanzalgrs@gmail.com>
2024-08-01 11:12:59 +0700

    Update app.test.js

--- apps/testing/app.test.js
+++ apps/testing/app.test.js
@@ -13,18 +13,18 @@ describe('Unit Test /', () => {
 });
 
 describe('Unit Test /app1', () => {
-  it('should respond with "Hello App1!"', async () => {
-    const response = await request(app).get('/app1');
-    expect(response.status).toBe(200);
+    it('should respond with "Hello App1!"', async () => {
+      const response = await request(app).get('/app1');
+      expect(response.status).toBe(200);
+    });
   });
-});
 
-describe('Unit Test /app2', () => {
-  it('should respond with "Hello App2!"', async () => {
-    const response = await request(app).get('/app2');
-    expect(response.status).toBe(200);
+  describe('Unit Test /app2', () => {
+    it('should respond with "Hello App2!"', async () => {
+      const response = await request(app).get('/app2');
+      expect(response.status).toBe(200);
+    });
   });
-});
 
 describe('Integration Test Connect Database', () => {
   beforeEach(() => {
